edf28f01815e2f0d5dc249d9eac2888bf9b1b8de
zhangli03 <zhangli03@meicai.cn>
2019-01-11 19:22:52 +0800

    。

--- app/src/main/java/com/zl/chat/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/zl/chat/ui/chat/ChatActivity.kt
@@ -3,7 +3,6 @@ package com.zl.chat.ui.chat
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
